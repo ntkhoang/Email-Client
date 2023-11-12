25b56a93b6aac7914b721ed4d9a4e68ace7d92e0
aimbs <109947519+hztran310@users.noreply.github.com>
2023-11-12 14:39:37 +0700

    changes in send_command and send_email
    can sent to, cc, bcc

--- MailClient-SMTP.py
+++ MailClient-SMTP.py
@@ -2,19 +2,34 @@ import socket
 
 def send_command(sock, command):
     sock.sendall(command.encode())
-    response = sock.recv(1024).decode()
-    print(response)
-    return response
-
-def send_email(smtp_server, from_address, to_address, subject, message):
+    response = sock.recv(1024)
+    if not response:
+        print("No response from server. The connection may have been closed.")
+        return
+    print(response.decode())
+    
+def send_email(smtp_server, from_address, to_address : tuple, cc_address : tuple, bcc_address : tuple, subject, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((smtp_server, 2225))
         send_command(sock, 'EHLO client\r\n')
         send_command(sock, f'MAIL FROM: <{from_address}>\r\n')
-        send_command(sock, f'RCPT TO: <{to_address}>\r\n')
+        for address in to_address:
+            send_command(sock, f'RCPT TO: <{address}>\r\n')
+        for address in cc_address:
+            send_command(sock, f'RCPT TO: <{address}>\r\n')
         send_command(sock, 'DATA\r\n')
-        send_command(sock, f'Subject: {subject}\r\n\r\n{message}\r\n.\r\n')
+        send_command(sock, f'Subject: {subject}\r\nTo: {",".join(to_address)}\r\nCc: {",".join(cc_address)}\r\n\r\n{message}\r\n.\r\n')
         send_command(sock, 'QUIT\r\n')
+        
+        for address in bcc_address:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((smtp_server, 2225))
+                send_command(sock, 'EHLO client\r\n')
+                send_command(sock, f'MAIL FROM: <{from_address}>\r\n')
+                send_command(sock, f'RCPT TO: <{address}>\r\n')
+                send_command(sock, 'DATA\r\n')
+                send_command(sock, f'Subject: {subject}\r\nTo: {",".join(to_address)}\r\nCc: {",".join(cc_address)}\r\nBCC: {address}\r\n\r\n{message}\r\n.\r\n')
+                send_command(sock, 'QUIT\r\n')
 
 if __name__ == "__main__":
     smtp_server = input("Nhập địa chỉ máy chủ SMTP: ")
