6d39077ddefa938bc3bfa52e6dc0ef3378e988d0
HaiDuongHuynhLe <114802302+haiduonghuynhle@users.noreply.github.com>
2023-11-14 14:55:48 +0700

    Feature 5 - Read email check

--- MailClient-POP3.py
+++ MailClient-POP3.py
@@ -51,19 +51,41 @@ def list_emails(sock, downloaded_ids): # List all emails on the server, with add
         if len(parts) == 2 and parts[1] not in downloaded_ids:
             print(f'Email ID: {parts[1]}')           
 
+def load_read_ids(): # Load read email IDs from file
+    if os.path.exists('read_ids.txt'):
+        with open('read_ids.txt', 'r') as file:
+            return file.read().splitlines()
+    else:
+        return []
+
+def save_read_id(id): # Save read email ID to file
+    with open('read_ids.txt', 'a') as file:
+        file.write(id + '\n')
+
+def list_emails(sock, downloaded_ids, read_ids): # List all emails on the server, with additional information about whether they are downloaded or read
+    response = send_command(sock, 'UIDL\r\n')
+    lines = response.split('\n')
+    for line in lines[1:]:
+        parts = line.split()
+        if len(parts) == 2:
+            status = 'đã đọc' if parts[1] in read_ids else 'chưa đọc'
+            print(f'Email ID: {parts[1]}, Trạng thái: {status}')
+
 if __name__ == "__main__":
     downloaded_ids = load_downloaded_ids()
+    read_ids = load_read_ids()
     pop3_server = input("Nhập địa chỉ máy chủ POP3: ")
     port = int(input("Nhập cổng kết nối: "))
     username = input("Nhập tên người dùng: ")
     password = input("Nhập mật khẩu: ")
     sock = connect_to_pop3_server(pop3_server, port)
     login(sock, username, password)
-    list_emails(sock, downloaded_ids)
+    list_emails(sock, downloaded_ids, read_ids)
     email_id = input("Nhập ID của email bạn muốn tải về: ")
     if email_id not in downloaded_ids:
         print(retrieve_email(sock, email_id))
         save_downloaded_id(email_id)
+        save_read_id(email_id)  # Mark as read.
     else:
         print("Email này đã được tải về trước đó.")
     quit(sock)
