712e7394a19eaea6bf700f0cc05796781e4c3720
aimbs <109947519+hztran310@users.noreply.github.com>
2023-11-21 09:42:05 +0700

    update format in send_email, send_email_with_multiple_attachments

--- MailClient.py
+++ MailClient.py
@@ -31,6 +31,7 @@ def send_command_with_print(sock, command):
         print(f"An error occurred: {e}")
         return ""
     
+    
 def send_email(smtp_server,smtp_port, from_address, to_address : tuple, cc_address : tuple, bcc_address : tuple, subject, message):
     current_time = datetime.datetime.now().strftime( '%d/%m/%Y %H:%M:%S' )
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -54,10 +55,12 @@ def send_email(smtp_server,smtp_port, from_address, to_address : tuple, cc_addre
         send_command_not_response(sock, f'Date: {current_time}\r\n')
         send_command_not_response(sock, 'MIME-Version: 1.0\r\n')
         send_command_not_response(sock, 'User-Agent: MailClient\r\n')
-        send_command_not_response(sock, f'To: {",".join(to_address)}\r\nCC: {",".join(cc_address)}\r\n')
-        send_command_not_response(sock, f'From: {from_address}\r\n')
+        send_command_not_response(sock, f'To: {",".join(to_address)}\r\n')
+        if len(cc_address) > 0:
+            send_command_not_response(sock, f'CC: {",".join(cc_address)}\r\n')
+        send_command_not_response(sock, f'From: <{from_address}>\r\n')
         send_command_not_response(sock, f'Subject: {subject}\r\n')
-        send_command_not_response(sock, f'Content-Type: text/plain; charset={"utf-8" if any(ord(c) > 127 for c in message) else "us-ascii"}, format=flowed\r\n')
+        send_command_not_response(sock, f'Content-Type: text/plain; charset={"utf-8" if any(ord(c) > 127 for c in message) else "us-ascii"}; format=flowed\r\n')
         send_command_not_response(sock, f'Content-Transfer-Encoding: {"base64" if any(ord(c) > 127 for c in message) else "7bit"}\r\n')
         send_command_not_response(sock, f'\r\n{message}\r\n.\r\n')
         send_command(sock, 'QUIT\r\n')
@@ -86,60 +89,6 @@ def generate_content_type_header(file_name):
 def generate_boundary():
     boundary = str(uuid.uuid4()).replace('-', '')
     return f'--------------{boundary}'
-    
-def send_email_with_attachment(smtp_server, smtp_port, from_address, to_address: tuple, cc_address: tuple, bcc_address: tuple, subject, message, attachment_file_name):
-    boundary = generate_boundary()
-    current_time = datetime.datetime.now().strftime( '%d/%m/%Y %H:%M:%S' )
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((smtp_server, smtp_port))
-
-            send_command(sock, 'EHLO example.com\r\n')
-            send_command(sock, f'MAIL FROM: <{from_address}>\r\n')
-
-            for address in to_address + cc_address + bcc_address:
-                send_command(sock, f'RCPT TO: <{address}>\r\n')
-
-            send_command(sock, 'DATA\r\n')
-
-            email_message = f'Content-Type: multipart/mixed; boundary="{boundary}"\r\n'
-            if '@' in from_address:
-                email_message += f"Message-ID: <{uuid.uuid4()}@{from_address.split('@')[1]}>\r\n"
-            else:
-                email_message += f"Message-ID: <{uuid.uuid4()}>\r\n"
-            email_message += f'Date: {current_time}\r\n'
-            email_message += 'MIME-Version: 1.0\r\n'
-            email_message += 'User-Agent: MailClient\r\n'
-            email_message += f'To: {", ".join(to_address)}\r\n'
-            email_message += f'CC: {", ".join(cc_address)}\r\n'
-            email_message += f'From: <{from_address}>\r\n'
-            email_message += f'Subject: {subject}\r\n'
-            email_message += '\r\nThis is a multi-part message in MIME format.\r\n'
-            email_message += f'{boundary}\r\n'
-
-            email_message += f'Content-Type: text/plain; charset={"UTF-8" if any(ord(c) > 127 for c in message) else "us-ascii"}, format=flowed\r\n'
-            email_message += f'Content-Transfer-Encoding: {"base64" if any(ord(c) > 127 for c in message) else "7bit"}\r\n\r\n'
-            email_message += f'{message}\r\n\r\n'
-            
-            email_message += f'{boundary}\r\n'
-            email_message += f'Content-Type: {generate_content_type_header(attachment_file_name)}; name="{attachment_file_name}"\r\n'
-            email_message += f'Content-Disposition: attachment; filename="{attachment_file_name}"\r\n'
-            email_message += f"Content-Transfer-Encoding: base64\r\n\r\n"
-
-            #In the MIME standard, base64 encoded data should be split into multiple lines, each containing no more than 76 characters.
-            with open(attachment_file_name, 'rb') as attachment:
-                attachment_data = base64.b64encode(attachment.read()).decode()
-                formatted_attachment_data = '\r\n'.join(attachment_data[i:i+76] for i in range(0, len(attachment_data), 76))
-                email_message += formatted_attachment_data
-
-            email_message += f'\r\n\r\n{boundary}\r\n.\r\n'
-            
-            # Send the email message
-            send_command(sock, email_message)
-
-
-    except Exception as e:
-        print(f"An error occurred while sending the email: {e}")
 
 def send_email_with_multiple_attachment(smtp_server, smtp_port, from_address, to_address : tuple, cc_address : tuple, bcc_address : tuple, subject, message, attachment_file_name : tuple):
     boundary = generate_boundary()
@@ -169,7 +118,8 @@ def send_email_with_multiple_attachment(smtp_server, smtp_port, from_address, to
             email_msg += 'MIME-Version: 1.0\r\n'
             email_msg += 'User-Agent: MailClient\r\n'
             email_msg += f'To: {", ".join(to_address)}\r\n'
-            email_msg += f'Cc: {", ".join(cc_address)}\r\n'
+            if len(cc_address) > 0:
+                email_msg += f'CC: {", ".join(cc_address)}\r\n'    
             email_msg += f'From: <{from_address}>\r\n'
             email_msg += f'Subject: {subject}\r\n'
             email_msg += '\r\n'
@@ -178,7 +128,7 @@ def send_email_with_multiple_attachment(smtp_server, smtp_port, from_address, to
             
             email_msg += f'Content-Type: text/plain; charset={"UTF-8" if any(ord(c) > 127 for c in message) else "us-ascii"}, format=flowed\r\n'
             email_msg += f'Content-Transfer-Encoding: 7bit\r\n'
-            email_msg += f'\r\n{message}\r\n\r\n'
+            email_msg += f'\r\n{message}\r\n'
             
             for file_name in attachment_file_name:
                 email_msg += f'{boundary}\r\n'
@@ -192,11 +142,11 @@ def send_email_with_multiple_attachment(smtp_server, smtp_port, from_address, to
                 email_msg += '\r\n'
                 with open(file_name, 'rb') as attachment:  # Corrected line
                     attachment_data = base64.b64encode(attachment.read()).decode()
-                    formatted_attachment_data = '\r\n'.join(attachment_data[i:i+76] for i in range(0, len(attachment_data), 76))
+                    formatted_attachment_data = '\r\n'.join(attachment_data[i:i+72] for i in range(0, len(attachment_data), 72))
                     email_msg += formatted_attachment_data
                 email_msg += '\r\n'
                     
-            email_msg += f'\r\n\r\n{boundary}\r\n.\r\n'
+            email_msg += f'\r\n\r\n{boundary}--\r\n.\r\n'
             send_command(sock, email_msg)
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -426,27 +376,17 @@ if __name__ == "__main__":
             choice = input("Input choice: ")
             if choice == 'y':
                 num_of_file = input("How many files do you want to attach? ")
-                if num_of_file == '1':
+                attachment_file_name = []
+                for i in range(int(num_of_file)):
                     check = False
                     while check == False:
-                        attachment_file_name = input("Input file name: ")
-                        if check_file_size(attachment_file_name) <= 3e+6:
-                            send_email_with_attachment(mail_server, smtp, username, to_address, cc_address, bcc_address, subject, message, attachment_file_name)
+                        file_name = input("Input file name: ")
+                        if check_file_size(file_name) <= 3e+6:
+                            attachment_file_name.append(file_name)
                             check = True
                         else:
-                            print("File size is too large")
-                else:
-                    attachment_file_name = []
-                    for i in range(int(num_of_file)):
-                        check = False
-                        while check == False:
-                            file_name = input("Input file name: ")
-                            if check_file_size(file_name) <= 3e+6:
-                                attachment_file_name.append(file_name)
-                                check = True
-                            else:
-                                print("File size is too large")                        
-                    send_email_with_multiple_attachment(mail_server, smtp, username, to_address, cc_address, bcc_address, subject, message, attachment_file_name)
+                            print("File size is too large")                        
+                send_email_with_multiple_attachment(mail_server, smtp, username, to_address, cc_address, bcc_address, subject, message, attachment_file_name)
             else:
                 send_email(mail_server, smtp, username, to_address, cc_address, bcc_address, subject, message)
             print("Send success")
